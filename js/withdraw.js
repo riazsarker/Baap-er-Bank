1e0ae0ef9a0d2119e5baa86a588c76ab8cbde984
riazsarker <riaz.cse260@gmail.com>
2023-02-11 23:20:42 +0600

    withdraw change

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,21 +2,32 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField = document.getElementById('total-withdraw');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    withdrawField.value= '';
+    if(isNaN(newWithdrawAmount)){
+        alert('please a provide a valid number');
+        return;
+    }
 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText=currentWithdrawTotal;
-    
+   
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
     
-    const currentBalanceTotal =previousBalanceTotal - newWithdrawAmount;
-    balanceTotalElement.innerText = currentBalanceTotal;
 
-    depositField.value= '';
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('baap er bank eto tk nai')
+        return;
+    }
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText=currentWithdrawTotal;
+    
+    const newBalanceTotal =previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 
+    
 })
